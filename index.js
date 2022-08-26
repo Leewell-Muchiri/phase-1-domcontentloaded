a813987896623050910dc69b95d6f3f8acaab4bf
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-27 01:14:21 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+//    console.log("This console.log() fires when index.js loads - before DOMContentLoaded is triggered")
+//    document.addEventListener("DOMContentLoaded", () => {
+// 	document.querySelector("p").textContent = "This is really cool!";
+// });
+document.addEventListener("DOMContentLoaded", () => {
+	document.querySelector("p").textContent = "This is really cool!";
+});
